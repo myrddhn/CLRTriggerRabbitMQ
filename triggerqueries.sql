b8c756c4329b9ea7f9e311ae45b580b752276d9f
Andi Reinbrech <andi@darwinistic.com>
2022-10-03 15:17:10 +0200

    Added queries in .sql file for Management Studio

---
+++ triggerqueries.sql
@@ -0,0 +1,40 @@
+use TriggerMQTest;
+--use TestDB;
+
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) [id]
+      ,[queue]
+      ,[message]
+  FROM [dbo].[QueueData]
+
+sp_configure 'clr enabled',1
+GO
+RECONFIGURE
+GO
+
+alter database testdb
+SET TRUSTWORTHY ON;
+go
+
+DROP TRIGGER TR_TriggerMQTest
+GO
+drop assembly [CLR_Triggers]
+GO
+CREATE ASSEMBLY [CLR_Triggers] FROM
+'D:\TFS\Andi\Projects\CLRTriggerRabbitMQ\TriggerCLRMQConnector\bin\Debug\TriggerCLRMQConnector.dll' WITH PERMISSION_SET=UNSAFE
+GO
+CREATE TRIGGER TR_TriggerMQTest
+ON dbo.QueueData
+FOR INSERT, UPDATE, DELETE    
+AS EXTERNAL NAME [CLR_Triggers].[CLRTriggers].QueueTrigger;
+GO
+
+drop assembly [System.Runtime.Serialization]
+
+CREATE ASSEMBLY [System.Runtime.Serialization] FROM
+'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Runtime.Serialization.dll' WITH PERMISSION_SET=UNSAFE
+GO
+
+UPDATE QueueData SET message = 'Hello world, it''s not working yet!!!' WHERE "queue" = '/trigger/two'
+
+select * from sys.dm_clr_properties
